b80ec2468ccb774130a787aed67158c356c14e49
Siddharth <siddharth.n.viswanath@gmail.com>
2023-01-04 19:36:07 -0600

    Completely finalized Weather Display

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import App from './App';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
